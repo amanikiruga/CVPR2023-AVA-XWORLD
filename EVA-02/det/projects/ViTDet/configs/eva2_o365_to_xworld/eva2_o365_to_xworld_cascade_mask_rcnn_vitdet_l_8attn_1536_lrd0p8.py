034e5b1962a6b5a1cb439547c1afc434357aedfb
Amani Kiruga <amanik@wifi-roaming-128-4-78-202.wifi.udel.edu>
2023-05-23 17:26:38 -0400

    remove .py from config

--- EVA-02/det/projects/ViTDet/configs/eva2_o365_to_xworld/eva2_o365_to_xworld_cascade_mask_rcnn_vitdet_l_8attn_1536_lrd0p8.py
+++ EVA-02/det/projects/ViTDet/configs/eva2_o365_to_xworld/eva2_o365_to_xworld_cascade_mask_rcnn_vitdet_l_8attn_1536_lrd0p8.py
@@ -1,6 +1,6 @@
 from functools import partial
 
-from ..common.xworld_loader_lsj.py import dataloader
+from ..common.xworld_loader_lsj import dataloader
 from .cascade_mask_rcnn_vitdet_b_100ep import (
     lr_multiplier,
     model,
@@ -14,33 +14,41 @@ from fvcore.common.param_scheduler import *
 from detectron2.solver import WarmupParamScheduler
 
 
-
-model.backbone.net.img_size = 1536  
-model.backbone.square_pad = 1536  
-model.backbone.net.patch_size = 16  
+model.backbone.net.img_size = 1536
+model.backbone.square_pad = 1536
+model.backbone.net.patch_size = 16
 model.backbone.net.window_size = 16
 model.backbone.net.embed_dim = 1024
 model.backbone.net.depth = 24
 model.backbone.net.num_heads = 16
-model.backbone.net.mlp_ratio = 4*2/3
+model.backbone.net.mlp_ratio = 4 * 2 / 3
 model.backbone.net.use_act_checkpoint = True
 model.backbone.net.drop_path_rate = 0.3
 
 # 2, 5, 8, 11, 14, 17, 20, 23 for global attention
 model.backbone.net.window_block_indexes = (
-    list(range(0, 2)) + list(range(3, 5)) + list(range(6, 8)) + list(range(9, 11)) + list(range(12, 14)) + list(range(15, 17)) + list(range(18, 20)) + list(range(21, 23))
+    list(range(0, 2))
+    + list(range(3, 5))
+    + list(range(6, 8))
+    + list(range(9, 11))
+    + list(range(12, 14))
+    + list(range(15, 17))
+    + list(range(18, 20))
+    + list(range(21, 23))
 )
 
-optimizer.lr=4e-5
-optimizer.params.lr_factor_func = partial(get_vit_lr_decay_rate, lr_decay_rate=0.8, num_layers=24)
+optimizer.lr = 4e-5
+optimizer.params.lr_factor_func = partial(
+    get_vit_lr_decay_rate, lr_decay_rate=0.8, num_layers=24
+)
 optimizer.params.overrides = {}
 optimizer.params.weight_decay_norm = None
 
 train.max_iter = 40000
 
-train.model_ema.enabled=True
-train.model_ema.device="cuda"
-train.model_ema.decay=0.9999
+train.model_ema.enabled = True
+train.model_ema.device = "cuda"
+train.model_ema.decay = 0.9999
 
 lr_multiplier = L(WarmupParamScheduler)(
     scheduler=L(CosineParamScheduler)(
@@ -51,5 +59,5 @@ lr_multiplier = L(WarmupParamScheduler)(
     warmup_factor=0.001,
 )
 
-dataloader.test.num_workers=0
-dataloader.train.total_batch_size=64
+dataloader.test.num_workers = 0
+dataloader.train.total_batch_size = 64
