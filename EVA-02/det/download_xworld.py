6dc8f3c12b719af843fb9b48cf3d7c3144bd7e81
Amani Kiruga <amanik@wifi-roaming-128-4-78-202.wifi.udel.edu>
2023-05-24 02:26:09 -0400

    test download xworld script

---
+++ EVA-02/det/download_xworld.py
@@ -0,0 +1,18 @@
+import gdown
+
+gdown.download(
+    f"https://drive.google.com/uc?export=download&confirm=pbef&id=1HxD43sB93K1o4YnbcUGA5Kf6kevfqMtK",
+    "/tmp/train_xworld.zip",
+)
+gdown.download(
+    f"https://drive.google.com/uc?export=download&confirm=pbef&id=1M8g-BAmwv8xzuvPvdhdCqyqBcEfjCGTR",
+    "/tmp/val_xworld.zip",
+)
+gdown.download(
+    f"https://drive.google.com/uc?export=download&confirm=pbef&id=12kqcvStd3L67V9PvGvbvszG9MEoNOY-7",
+    "/tmp/train_xworld.json",
+)
+gdown.download(
+    f"https://drive.google.com/uc?export=download&confirm=pbef&id=1wKNESLeURHeW0XBwYMFsrjqwNPaTmtC5",
+    "/tmp/val_xworld.json",
+)
